421aa731a1ed46d8b008a063dc3ed40664e3b95d
ryik5 <ryik.yuri@gmail.com>
2020-03-13 17:13:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTAService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var service = new AstaServiceLocal();
+            ServiceBase[] ServicesToRun;
+
+            ServicesToRun = new ServiceBase[]
+            {
+                service
+            };
+
+            if (Environment.UserInteractive)
+            {
+                // Разбор пути для саморегистрации
+                if (args != null && args[0].Length > 1
+                    && (args[0].StartsWith("-") || args[0].StartsWith("/")))
+                {
+                    switch (args[0].Substring(1).ToLower())
+                    {
+                        case "install":
+                        case "i":
+                            if (!ServiceInstallerUtility.Install())
+                                Console.WriteLine("Failed to install service");
+                            else
+                            {
+                                service.Start();
+                                Console.WriteLine("Running service");
+                            }
+                            break;
+                        case "uninstall":
+                        case "u":
+                            service.Stop();
+                            if (!ServiceInstallerUtility.Uninstall())
+                                Console.WriteLine("Failed to uninstall service");
+                            else 
+                                Console.WriteLine("Service stopped. Goodbye.");
+                            //"taskkill /f /IM astaservice.exe";
+                            break;
+                        default:
+                            // ServiceInstallerUtility.Install();
+                            break;
+                    }
+                }
+
+                //Console.CancelKeyPress += (x, y) => service.Stop();
+                //ServiceInstallerUtility.Install();
+                //Console.WriteLine("Running service, press a key to stop");
+                //Console.ReadKey();
+                //service.Stop();
+                //Console.WriteLine("Service stopped. Goodbye.");
+            }
+            else
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASTAService
+{
+    public partial class AstaServiceLocal : ServiceBase
+    {
+        private readonly Thread workerThread;
+        public AstaServiceLocal()
+        {
+            InitializeComponent();
+            workerThread = new Thread(DoWork);
+            workerThread.SetApartmentState(ApartmentState.STA);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            workerThread.Start();
+        }
+
+        internal void Start()
+        {
+            workerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            workerThread.Abort();
+        }
+
+        private static void DoWork()
+        {
+            while (true)
+            {
+                // log.Info("Doing work...");
+                // do some work, then
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+
+
+    // Утилита саморегистрации
+    [RunInstaller(true)]
+    public partial class ServiceInstallerUtility : Installer
+    {
+      //  private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        private static readonly string exePath = Assembly.GetExecutingAssembly().Location;
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public ServiceInstallerUtility()
+        {
+           // InitializeComponent();
+            processInstaller = new ServiceProcessInstaller();
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller = new ServiceInstaller();
+            serviceInstaller.StartType = ServiceStartMode.Manual;           
+            serviceInstaller.ServiceName = "AstaServiceLocal";
+            serviceInstaller.DisplayName = "ASTA Local Service";
+            serviceInstaller.Description = "ASTA (get and send data) as a Local Service";
+
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+
+        public static bool Install()
+        {
+            try { 
+                ManagedInstallerClass.InstallHelper(new[] { exePath }); 
+            }
+            catch { return false; }
+            return true;
+        }
+
+        public static bool Uninstall()
+        {
+            try { 
+                ManagedInstallerClass.InstallHelper(new[] { "/u", exePath }); 
+            }
+            catch { return false; }
+            return true;
+        }
+    }
+}
