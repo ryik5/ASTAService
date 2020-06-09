d2f49f3f1068642271e6c99257f3e5688ec3f2ff
ryik5 <ryik.yuri@gmail.com>
2020-06-09 17:56:59 +0300

    study

--- Service1.cs
+++ Service1.cs
@@ -1,35 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.Data;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Timers;
 
 namespace ASTAService
 {
     public partial class AstaServiceLocal : ServiceBase
     {
-        private readonly Thread workerThread;
-        //or Logger logger;
+        private System.Timers.Timer timer = null;
+        private Thread workerThread;
+        Logger log;
+
+
         public AstaServiceLocal()
         {
             InitializeComponent();
-            workerThread = new Thread(DoWork);
-            workerThread.SetApartmentState(ApartmentState.STA);
-            //or workerThread = new Thread(new ThreadStart(logger.Start));
         }
+        
 
         protected override void OnStart(string[] args)
         {
+            workerThread = new Thread(new ThreadStart( DoWork));
+            workerThread.SetApartmentState(ApartmentState.STA);
+            //or workerThread = new Thread(new ThreadStart(logger.Start));
             workerThread.Start();
+            workerThread.IsBackground = true;
         }
 
         internal void Start()
@@ -39,52 +38,80 @@ namespace ASTAService
 
         protected override void OnStop()
         {
+            log.Stop();
             workerThread.Abort();
         }
 
-        private static void DoWork()
+        private  void DoWork()
         {
-            while (true)
-            {
-              //  MessageBox.Show("Start");
-                // log.Info("Doing work...");
-                // do some work, then
-                Thread.Sleep(5000);
-            }
+            timer = new System.Timers.Timer(30000);//создаём объект таймера
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            //form1.Show();
+            //System.Windows.Forms.Application.Run(form1);
+
+            log = new Logger();
+            log.Start();
+
+            //while (true)
+            //{
+                
+            //    //  MessageBox.Show("Start");
+            //    // log.Info("Doing work...");
+            //    // do some work, then
+            //    Thread.Sleep(1000);
+            //}
+
+
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Enabled = false;
+            timer.Stop();
+            // runProcedure(); //Запускаем процедуру (чего хотим выполнить по таймеру).
+            timer.Enabled = true;
+            timer.Start();
         }
     }
 
-        /// <summary>
-        /// Утилита саморегистрации
-        /// </summary>
+
+    /// <summary>
+    /// Утилита саморегистрации
+    /// </summary>
     [RunInstaller(true)]
     public partial class ServiceInstallerUtility : Installer
     {
-      //  private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        //https://www.c-sharpcorner.com/article/installing-a-service-programmatically/
+        //https://www.csharp-examples.net/install-net-service/
+        //https://stackoverflow.com/questions/12201365/programmatically-remove-a-service-using-c-sharp
+
+        //  private static readonly ILog log = LogManager.GetLogger(typeof(Program));
         private static readonly string exePath = Assembly.GetExecutingAssembly().Location;
         ServiceInstaller serviceInstaller;
         ServiceProcessInstaller processInstaller;
 
         public ServiceInstallerUtility()
         {
-           // InitializeComponent();
+            //InitializeComponent();
             processInstaller = new ServiceProcessInstaller();
             processInstaller.Account = ServiceAccount.LocalSystem;
 
             serviceInstaller = new ServiceInstaller();
-            serviceInstaller.StartType = ServiceStartMode.Manual;           
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
             serviceInstaller.ServiceName = "AstaServiceLocal";
             serviceInstaller.DisplayName = "ASTA Local Service";
             serviceInstaller.Description = "ASTA (get and send data) as a Local Service";
 
             Installers.Add(processInstaller);
             Installers.Add(serviceInstaller);
+
         }
 
         public static bool Install()
         {
-            try { 
-                ManagedInstallerClass.InstallHelper(new[] { exePath }); 
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new[] { "/i", exePath });
             }
             catch { return false; }
             return true;
@@ -92,13 +119,20 @@ namespace ASTAService
 
         public static bool Uninstall()
         {
-            try { 
-                ManagedInstallerClass.InstallHelper(new[] { "/u", exePath }); 
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new[] { "/u", exePath });
+                //ServiceInstaller ServiceInstallerObj = new ServiceInstaller();
+                //InstallContext Context = new InstallContext("<<log file path>>", null);
+                //ServiceInstallerObj.Context = Context;
+                //ServiceInstallerObj.ServiceName = "AstaServiceLocal";
+                //ServiceInstallerObj.Uninstall(null);
             }
             catch { return false; }
             return true;
         }
     }
+   
     public class Logger
     {
         FileSystemWatcher watcher;
@@ -161,11 +195,42 @@ namespace ASTAService
             {
                 using (StreamWriter writer = new StreamWriter("D:\\templog.txt", true))
                 {
-                    writer.WriteLine(String.Format("{0} файл {1} был {2}",
-                        DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.WriteLine($"{DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss")} файл {filePath} был {fileEvent}");
                     writer.Flush();
                 }
             }
         }
     }
+
+    /*      
+using System.Timers;
+namespace MyService
+{
+    {
+    public partial class MyService: ServiceBase
+    {
+    private System.Timers.Timer timer = null;
+
+    public MyService()
+    {
+        InitializeComponent();
+        timer = new System.Timers.Timer(30000);//создаём объект таймера
+        timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+    }
+
+    void timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        //собственно здесь пишем код таймера
+    }
+    protected override void OnStart(string[] args)
+    {
+        timer.Start();
+    }
+
+    protected override void OnStop()
+    {
+        timer.Stop();
+    }
+} 
+*/
 }
