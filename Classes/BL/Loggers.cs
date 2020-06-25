681cc611fc6ff7e0a48144339118829691f27e19
ryik5 <ryik.yuri@gmail.com>
2020-06-25 16:48:58 +0300

    changed logging to a Log4net style

--- Classes/BL/Loggers.cs
+++ Classes/BL/Loggers.cs
@@ -88,20 +88,40 @@ namespace ASTAWebClient
 
         public Logger() { }
 
-        public void WriteString(string text, string eventText="Message")
+        public void WriteString(string text, string eventText = "Message")
         {
-            RecordEntry(eventText, text);
+            RecordEntry(text, eventText);
         }
-        private void RecordEntry(string eventText, string text)
+        private void RecordEntry(string text, string eventText)
         {
-            string path = Assembly.GetExecutingAssembly().Location;
-            string pathToLog = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileNameWithoutExtension(path) + ".log");
-            lock (obj)
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string pathToLogDir, pathToLog;
+            try
             {
-                using (System.IO.StreamWriter writer = new System.IO.StreamWriter(pathToLog, true))
+                pathToLogDir = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), $"logs");
+                if (!System.IO.Directory.Exists(pathToLogDir))
+                     System.IO.Directory.CreateDirectory(pathToLogDir); 
+
+                pathToLog = System.IO.Path.Combine(pathToLogDir, $"{DateTime.Now.ToString("yyyy-MM-dd")}.log");
+                lock (obj)
                 {
-                    writer.WriteLine($"{DateTime.Now.ToString("yyyy.MM.dd|hh:mm:ss")}|{eventText}|{text}");
-                    writer.Flush();
+                    using (System.IO.StreamWriter writer = new System.IO.StreamWriter(pathToLog, true))
+                    {
+                        writer.WriteLine($"{DateTime.Now.ToString("yyyy.MM.dd|hh:mm:ss")}|{eventText}|{text}");
+                        writer.Flush();
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                pathToLog = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileNameWithoutExtension(path) + ".log");
+                lock (obj)
+                {
+                    using (System.IO.StreamWriter writer = new System.IO.StreamWriter(pathToLog, true))
+                    {
+                        writer.WriteLine($"{DateTime.Now.ToString("yyyy.MM.dd|hh:mm:ss")}|{err.ToString()}");
+                        writer.Flush();
+                    }
                 }
             }
         }
